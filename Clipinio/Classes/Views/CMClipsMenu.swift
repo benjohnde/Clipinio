15ae2eab9e1a3cbd7cea9e8ff45fcc79f981610f
i504157 <benjamin.john@sap.com>
2019-02-15 12:51:33 +0100

    ADD: bumped version increment and simplified some Optionals

--- Clipinio/Classes/Views/CMClipsMenu.swift
+++ Clipinio/Classes/Views/CMClipsMenu.swift
@@ -25,13 +25,13 @@ class CMClipsMenu: NSObject {
         clear()
         delegate.pasteboard.clips.enumerated().forEach {
             let title = String($0.0) + ". " + $0.1.preview
-            delegate.__clipsMenu!.insertItem(withTitle: title, action: nil, keyEquivalent: "", at: $0.0 + 2)
+            delegate.__clipsMenu.insertItem(withTitle: title, action: nil, keyEquivalent: "", at: $0.0 + 2)
         }
     }
     
     fileprivate func clear() {
-        while delegate.__clipsMenu!.items.count > 2 {
-            delegate.__clipsMenu!.removeItem(at: delegate.__clipsMenu!.items.count - 1)
+        while delegate.__clipsMenu.items.count > 2 {
+            delegate.__clipsMenu.removeItem(at: delegate.__clipsMenu.items.count - 1)
         }
     }
 }
